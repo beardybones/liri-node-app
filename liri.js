e95e681201ff5c18da2e8809e589cfdaa7a77f19
beardybones <beardybonescodes@gmail.com>
2018-12-08 00:44:54 -0800

    finished

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var fs = require("fs");
+var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
@@ -12,14 +13,21 @@ var userInput = process.argv.slice(3).join(" ");
 if (process.argv[2] === "concert-this") {
     console.log(userInput);
     var bandURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
-    console.log(bandURL);
+    // moment.js
+    // no wifi!
+    //npm install moment --save
+
+    
+    // console.log(bandURL);
     axios
     .get(bandURL)
     .then(function (response) {
-        console.log(response.data);
-        // console.log("Venue: " + response.data);
-        // console.log("Location: " + response.data);
-        // console.log("Date: " + response.data);
+        // console.log(response.data);
+        var bandDate = response.data[0].datetime;
+    var momentDate = moment(bandDate).format("L");
+        console.log("Venue: " + response.data[0].venue.name);
+        console.log("Location: " + response.data[0].venue.city);
+        console.log("Date: " + momentDate);
 
     })
     .catch(function (error) {
@@ -80,13 +88,18 @@ if (process.argv[2] === "movie-this") {
 // ---------------------------------------------------------------------
 
 if (process.argv[2] === "spotify-this-song") {
-    spotify.search({ type: 'track', query: songName, limit: 5}, function(err, data) {
+    spotify.search({ type: 'track', query: userInput, limit: 5}, function(err, data) {
+        if (!userInput) {
+            userInput = "The+Sign";
+        }
         if (err) {
             return console.log('Error occurred: ' + err);
         }   
            
-
-        console.log(data.tracks.items[0].name); 
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].preview_url);
+        console.log(data.tracks.items[0].album.name);
         });
 }
 
@@ -100,6 +113,26 @@ if (process.argv[2] === "spotify-this-song") {
 // ---------------------------------------------------------------------
 
 if (process.argv[2] === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        spotify.search({ type: 'track', query: userInput, limit: 5}, function(err, data) {
+            
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }   
+               
+            console.log(data.tracks.items[0].artists[0].name);
+            console.log(data.tracks.items[0].name);
+            console.log(data.tracks.items[0].preview_url);
+            console.log(data.tracks.items[0].album.name);
+            });
+        if (error) {
+            return console.log(error);
+          }
+        
+          console.log(data);
+        
+          
+    });
 
 }
 // ---------------------------------------------------------------------
